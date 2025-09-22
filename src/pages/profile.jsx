76f70c505f732dc7e5ef7eb6580e3c95cdea5bdd
Patricio Pena pc <patricio17po@gmail.com>
2025-09-22 14:09:58 -0600

    Add-ProfilePage

---
+++ src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../components/navBar";
+
+const profile = () => {
+  return (
+    
+    <div>
+        <Navbar title="Perfil" />
+      <p>Hola </p>
+    </div>
+  )
+}
+
+export default profile
